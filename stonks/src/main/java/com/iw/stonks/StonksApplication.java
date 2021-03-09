d64f16fa851d148e0ee12b8fda232201867d4cb2
Alberto Redondo <albertomost@gmail.com>
2021-03-09 13:14:21 +0100

    Base del proyecto

---
+++ stonks/src/main/java/com/iw/stonks/StonksApplication.java
@@ -0,0 +1,13 @@
+package com.iw.stonks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StonksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StonksApplication.class, args);
+	}
+
+}
---
+++ stonks/src/test/java/com/iw/stonks/StonksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iw.stonks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StonksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
