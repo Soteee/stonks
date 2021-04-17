68e191cd9920eb73750c9098bd15f12b976b8f62
Victor <victorfrespe@gmail.com>
2021-04-17 17:40:54 +0200

    Movidas del login

--- src/main/java/es/ucm/fdi/stonks/LoginSuccessHandler.java
+++ src/main/java/es/ucm/fdi/stonks/LoginSuccessHandler.java
@@ -66,6 +66,6 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 		// redirects to 'admin' or 'user/{id}', depending on the user
 		response.sendRedirect(u.hasRole(User.Role.ADMIN) ? 
 				"admin/" :
-				"user/" + u.getId());
+				"u/" + u.getId());
 	}
 }
