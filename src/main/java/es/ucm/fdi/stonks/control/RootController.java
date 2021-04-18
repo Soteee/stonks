0d1a54b7e08dc56fd05d2f22f2ee04eabb823c5d
Victor <victorfrespe@gmail.com>
2021-04-18 13:01:42 +0200

    Arreglados algunos bugs por no haber cambiado el nombre de logIn a login en todas partes xd

--- src/main/java/es/ucm/fdi/stonks/control/RootController.java
+++ src/main/java/es/ucm/fdi/stonks/control/RootController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.view.RedirectView;
 
 import javax.persistence.EntityManager;
 import javax.servlet.http.HttpServletResponse;
@@ -138,7 +137,7 @@ public class RootController {
         newUser.setRoles("USER");
         entityManager.persist(newUser);
 
-        return "logIn";
+        return "login";
     }
 
     @GetMapping("/createRoom")
