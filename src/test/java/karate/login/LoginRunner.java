8d975a50edca3719424659ca1c65a64a61c94e3a
Alberto Redondo <albertomost@gmail.com>
2021-04-19 20:31:26 +0200

    Añadido test karate

---
+++ src/test/java/karate/login/LoginRunner.java
@@ -0,0 +1,11 @@
+package karate.login;
+
+import com.intuit.karate.junit5.Karate;
+
+class PersonRunner {
+
+    @Karate.Test
+    Karate testAll() {
+        return Karate.run().relativeTo(getClass());
+    }
+}
