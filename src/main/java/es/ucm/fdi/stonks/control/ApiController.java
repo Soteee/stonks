cefda41f66f4ab81a4ea2d61d1acac0c26122f8e
Victor <victorfrespe@gmail.com>
2021-04-26 14:04:18 +0200

    Creado roomsController

--- src/main/java/es/ucm/fdi/stonks/control/ApiController.java
+++ src/main/java/es/ucm/fdi/stonks/control/ApiController.java
@@ -23,8 +23,7 @@ import javax.transaction.Transactional;
 
 import org.json.JSONObject;
 
-@Data
-@Controller("action_buy")
+@Controller
 public class ApiController {
     @Autowired
     private EntityManager entityManager;
